e12440ab43c18b03af6b5a391c186fea7defb6f3
Shivam Srivastava <9876srivastavashiavm@gmail.com>
2023-09-18 12:31:40 +0530

    Initial commit including forgot password feature

---
+++ src/features/order/Order.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  
+} from './orderSlice';
+
+export function Order() {
+  const dispatch = useDispatch();
+
+
+  return (
+    <div>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/ProductDetailPage.js
@@ -0,0 +1,16 @@
+import Footer from "../features/common/Footer";
+import NavBar from "../features/navbar/Navbar";
+import ProductDetail from "../features/product-list/components/ProductDetail";
+
+function ProductDetailPage() {
+    return (
+        <div>
+            <NavBar>
+                <ProductDetail></ProductDetail>
+            </NavBar>
+            <Footer></Footer>
+        </div>
+    );
+}
+
+export default ProductDetailPage;
